18e03d7a627676f114f86f1862bef035776e5f24
MoskowitzRocket <kalebug@gmail.com>
2025-02-12 14:51:41 -0800

    new implementation added -> I like it more todo: comment QuadraticProbing.h and BiMap.h

--- BiMap.h
+++ BiMap.h
@@ -1,301 +1,300 @@
-#ifndef QUADRATIC_PROBING_H
-#define QUADRATIC_PROBING_H
-
 #include <vector>
 #include <algorithm>
 #include <functional>
-#include <iostream>
 #include <string>
+#include <iostream>
 using namespace std;
 
-int nextPrime(int n);
+int nextPrime(int n );
 
-// QuadraticProbing Hash table 
-//
-// CONSTRUCTION: an approximate initial size or default of 101
-//
-// ******************PUBLIC OPERATIONS*********************
-// bool insert( x )       --> Insert x
-// bool removeKey( x )       --> Remove x
-// bool containsKey( x )     --> Return true if x is present
-// void makeEmpty( )      --> Remove all items
-// int hashCode( string str ) --> Global method to hash strings
-
-template <typename KeyType, typename ValType>
+// Bijective Map class
+// Implemented with two hash tables.
+template <typename KeyType, typename ValType> //
 class BiMap
 {
 public:
-
-    //Constructor
-    //Sizes array to next prime greater than size -> default 101
-    //Then sets all entries to 0
-
-    explicit BiMap(int size = 11) : array(nextPrime(size)), valArray(nextPrime(size))
+    BiMap() : keyToVal(), valToKey()
     {
-        makeEmpty();
     }
-
-    //sets current size to 0 and sets all array firsts to EMPTY
+    // constructor
     void makeEmpty()
     {
-        currentSize = 0;
-        for (auto& entry : array)
-            entry.info = EMPTY;
-
-        for (auto& entry : valArray)
-            entry.info = EMPTY;
+        keyToVal.makeEmpty();
+        valToKey.makeEmpty();
     }
-
+    // remove all pairs
     int getSize() const
     {
-        return currentSize;
+        return keyToVal.getSize(); // Return instead of printing
     }
 
-    bool insert(const KeyType& x, const ValType& y)
+    // returns the current number of pairs
+    bool insert(const KeyType &x, const ValType &y)
     {
-        // Insert x as active
-        int currentPos = findPosKey(x);
-        int currentPosVal = findPosVal(y);
+        if (keyToVal.contains(x) || valToKey.contains(y))
+            return false; // Prevent duplicate keys/values
 
-        //int currentPosVal = findPosVal( y );
+        keyToVal.insert(x, y);
+        valToKey.insert(y, x);
+        return true;
+    }
 
-        if (isActive(currentPos, array) || isActive(currentPosVal, valArray))
-        {
+    // insert pair <x,y>, provided x is not the
+    // key of a current pair and y is not the value of
+    // a current pair.  Return true iff <x,y> was inserted.
+    bool containsKey(const KeyType &x) const
+    {
+        return keyToVal.contains(x); // Return the result
+    }
+
+    bool containsVal(const ValType &x) const
+    {
+        return valToKey.contains(x); // Return the result
+    }
+
+    bool removeKey(const KeyType &x)
+    {
+        if (!keyToVal.contains(x)) // Check if key exists
             return false;
-        }
 
-        if (array[currentPos].info != DELETED && valArray[currentPosVal].info != DELETED) {
-            ++currentSize;
-        }
-
-
-        array[currentPos].first = x;
-        array[currentPos].second = y;
-        array[currentPos].info = ACTIVE;
-
-        valArray[currentPosVal].first = x;
-        valArray[currentPosVal].second = y;
-        valArray[currentPosVal].info = ACTIVE;
-
-        // Rehash; see Section 5.5
-        if (currentSize > array.size() / 2) {
-            rehash();
-        }
+        ValType val = keyToVal.getValue(x); // Get the associated value
+        keyToVal.remove(x);                 // Remove from key-to-value map
+        valToKey.remove(val);               // Remove from value-to-key map
 
         return true;
     }
 
-    //calls find pos to find index of x
-    //uses is active to make sure its ACTIVE -> not deleted or empty
-
-    bool containsKey(const KeyType& x) const
+    bool removeVal(const ValType &x)
     {
-        int pos = findPosKey(x);
-        return isActive(pos, array);
-    }
-
-    bool containsVal(const ValType& y) const
-    {
-        //TODO
-        int pos = findPosVal(y);
-        return isActive(pos, valArray);
-    }
-
-    bool removeKey(const KeyType& x)
-    {
-        int currentPos = findPosKey(x);
-        if (!isActive(currentPos, array))
+        if (!valToKey.contains(x)) // Check if value exists
             return false;
 
-        // Remove value associated with the key
-        ValType temp = array[currentPos].second;
-        array[currentPos].info = DELETED;  // Mark the key as DELETED first
+        KeyType key = valToKey.getValue(x); // Get the associated key
+        valToKey.remove(x);                 // Remove from value-to-key map
+        keyToVal.remove(key);               // Remove from key-to-value map
 
-        // Now, safely remove the value from valArray
-        removeVal(temp);
-        
         return true;
     }
 
-    bool removeVal(const ValType& y)
+    const ValType &getVal(const KeyType &x) const
     {
-        int currentPos = findPosVal(y);
-        if (!isActive(currentPos, valArray))
-            return false;
+        if (!keyToVal.contains(x))
+            throw runtime_error("Key not found!");
 
-        // Remove key associated with the value
-        KeyType temp = valArray[currentPos].first;
-        valArray[currentPos].info = DELETED;  // Mark the value as DELETED first
-
-        // Now, safely remove the key from array
-        removeKey(temp);
-        currentSize--;
-        return true;
+        return keyToVal.getValue(x); // Assuming HMap provides getValue()
     }
 
-    const ValType& getVal(const KeyType& x) const
+    const KeyType &getKey(const ValType &x) const
     {
-        int pos = findPosKey(x);
-        return array[pos].second;
+        if (!valToKey.contains(x))
+            throw runtime_error("Value not found!");
+
+        return valToKey.getValue(x); // Assuming HMap provides getValue()
     }
 
-    const KeyType& getKey(const ValType& y) const
+    void display()
     {
-        int pos = findPosVal(y);
-        return valArray[pos].first;
+        keyToVal.display();
     }
 
-    enum EntryType { ACTIVE, EMPTY, DELETED };
-
-    void ddisplay() const
+    void ddisplay()
     {
-        cout << "KeyArray\n";
-        cout << "Index\tStatus\tFirst\tSecond" << endl;
-        for (size_t i = 0; i < array.size(); ++i)
-        {
-            cout << i << "\t";
-            switch (array[i].info)
-            {
-            case ACTIVE:
-                cout << "ACTIVE\t" << array[i].first << "\t" << array[i].second;
-                break;
-            case EMPTY:
-                cout << "EMPTY\t-\t-";
-                break;
-            case DELETED:
-                cout << "DELETED\t-\t-";
-                break;
-            }
-            cout << endl;
-        }
-        cout << "ValArray\n";
-        cout << "Index\tStatus\tFirst\tSecond" << endl;
-        for (size_t i = 0; i < valArray.size(); ++i)
-        {
-            cout << i << "\t";
-            switch (valArray[i].info)
-            {
-            case ACTIVE:
-                cout << "ACTIVE\t" << valArray[i].first << "\t" << valArray[i].second;
-                break;
-            case EMPTY:
-                cout << "EMPTY\t-\t-";
-                break;
-            case DELETED:
-                cout << "DELETED\t-\t-";
-                break;
-            }
-            cout << endl;
-        }
+        cout << "Key to Val Array\n";
+        keyToVal.ddisplay();
+        cout << "Val to Key Array\n";
+        valToKey.ddisplay();
     }
 
 private:
-    struct HashEntry
-    {
-        KeyType first;
-        ValType second;
-        EntryType info;
 
-        HashEntry(const KeyType& e = KeyType{ }, EntryType i = EMPTY)
-            : first{ e }, info{ i } {
+    template <typename kt, typename vt>
+    class HMap
+    {
+    public:
+        explicit HMap(int size = 11) : array(nextPrime(size))
+        {
+            makeEmpty();
         }
 
-        HashEntry(KeyType&& e, EntryType i = EMPTY)
-            : first{ std::move(e) }, info{ i } {
+        bool contains(const kt &x) const
+        {
+            return isActive(findPos(x));
+        }
+
+        void makeEmpty()
+        {
+            currentSize = 0;
+            for (auto &entry : array)
+                entry.info = EMPTY;
+        }
+
+        int getSize() const
+        {
+            return currentSize;
+        }
+
+        bool insert(const kt &x, const vt &y)
+        {
+            // Insert x as active
+            int currentPos = findPos(x);
+            if (isActive(currentPos))
+                return false;
+
+            if (array[currentPos].info != DELETED)
+                ++currentSize;
+
+            array[currentPos].key = x;
+            array[currentPos].value = y;
+            array[currentPos].info = ACTIVE;
+
+            // Rehash; see Section 5.5
+            if (currentSize > array.size() / 2)
+                rehash();
+
+            return true;
+        }
+
+        bool insert(kt &&x)
+        {
+            // Insert x as active
+            int currentPos = findPos(x);
+            if (isActive(currentPos))
+                return false;
+
+            if (array[currentPos].info != DELETED)
+                ++currentSize;
+
+            array[currentPos] = std::move(x);
+            array[currentPos].info = ACTIVE;
+
+            // Rehash; see Section 5.5
+            if (currentSize > array.size() / 2)
+                rehash();
+
+            return true;
+        }
+
+        bool remove(const kt &x)
+        {
+            int currentPos = findPos(x);
+            if (!isActive(currentPos))
+                return false;
+
+            array[currentPos].info = DELETED;
+            return true;
+        }
+
+        const vt &getValue(const kt &x) const
+        {
+            int pos = findPos(x); // Find the index of the key in the hash table
+            return array[pos].value;
+        }
+
+        enum EntryType
+        {
+            ACTIVE,
+            EMPTY,
+            DELETED
+        };
+
+        void display()
+        {
+            for (const auto &entry : array)
+            {
+                if (entry.info == ACTIVE)
+                    cout << "(" << entry.key << "," << entry.value << ") ";
+            }
+            cout << endl;
+        }
+
+        void ddisplay()
+        {
+            {
+                cout << "Index\tStatus\tFirst\tSecond" << endl;
+                for (size_t i = 0; i < array.size(); ++i)
+                {
+                    cout << i << "\t";
+                    switch (array[i].info)
+                    {
+                    case ACTIVE:
+                        cout << "ACTIVE\t" << array[i].key << "\t" << array[i].value;
+                        break;
+                    case EMPTY:
+                        cout << "EMPTY\t-\t-";
+                        break;
+                    case DELETED:
+                        cout << "DELETED\t-\t-";
+                        break;
+                    }
+                    cout << endl;
+                }
+            }
+        }
+
+    private:
+        struct HashEntry
+        {
+            kt key;
+            vt value;
+            EntryType info;
+
+            HashEntry(const kt &e = kt{}, EntryType i = EMPTY)
+                : key{e}, info{i} {}
+
+            HashEntry(kt &&e, EntryType i = EMPTY)
+                : key{std::move(e)}, info{i} {}
+        };
+
+        vector<HashEntry> array;
+        int currentSize;
+
+        bool isActive(int currentPos) const
+        {
+            return array[currentPos].info == ACTIVE;
+        }
+
+        int findPos(const kt &x) const
+        {
+            int offset = 1;
+            int currentPos = myhash(x);
+
+            while (array[currentPos].info != EMPTY &&
+                   array[currentPos].key != x)
+            {
+                currentPos += offset; // Compute ith probe
+                offset += 2;
+                if (currentPos >= array.size())
+                    currentPos -= array.size();
+            }
+
+            return currentPos;
+        }
+
+        void rehash()
+        {
+            vector<HashEntry> oldArray = array;
+
+            // Create new double-sized, empty table
+            array.resize(nextPrime(2 * oldArray.size()));
+            for (auto &entry : array)
+                entry.info = EMPTY;
+
+            // Copy table over
+            currentSize = 0;
+            for (auto &entry : oldArray)
+                if (entry.info == ACTIVE)
+                    insert(std::move(entry.key));
+        }
+
+        size_t myhash(const kt &x) const
+        {
+            static hash<kt> hf;
+            return hf(x) % array.size();
         }
     };
 
-    vector<HashEntry> array;
-    vector<HashEntry> valArray;
+    HMap<KeyType, ValType> keyToVal;
+    HMap<ValType, KeyType> valToKey;
+
     int currentSize;
-
-    bool isActive(int currentPos, const vector<HashEntry>& arr) const
-    {
-        return arr[currentPos].info == ACTIVE;
-    }
-
-    int findPosKey(const KeyType& x) const
-    {
-        int offset = 1;
-        // int currentPos = myhashKey(x);
-        int currentPos = myhash(x);
-        while (array[currentPos].info != EMPTY &&
-            array[currentPos].first != x)
-        {
-            currentPos += offset;  // Compute ith probe
-            offset += 2;
-            if (currentPos >= array.size())
-                currentPos -= array.size();
-        }
-
-        return currentPos;
-    }
-
-    int findPosVal(const ValType& y) const
-    {
-        int offset = 1;
-        //int currentPos = myhashVal(y);
-        int currentPos = myhash(y);
-        while (valArray[currentPos].info != EMPTY &&
-            valArray[currentPos].second != y)
-        {
-            currentPos += offset;  // Compute ith probe
-            offset += 2;
-            if (currentPos >= valArray.size())
-                currentPos -= valArray.size();
-        }
-
-        return currentPos;
-    }
-
-
-
-    void rehash()
-    {
-        vector<HashEntry> oldArray = array;
-        vector<HashEntry> oldValArray = valArray;
-
-
-        // Create new double-sized, empty table
-        array.resize(nextPrime(2 * oldArray.size()));
-        valArray.resize(nextPrime(2 * oldValArray.size()));
-
-        for (auto& entry : array)
-            entry.info = EMPTY;
-
-        for (auto& entry : valArray)
-            entry.info = EMPTY;
-
-        // Copy table over
-        currentSize = 0;
-        for (auto& entry : oldArray)
-            if (entry.info == ACTIVE)
-                insert(std::move(entry.first), std::move(entry.second));
-
-        for (auto& entry : oldValArray)
-            if (entry.info == ACTIVE)
-                insert(std::move(entry.first), std::move(entry.second));
-    }
-
-    size_t myhashKey(const KeyType& x) const
-    {
-        static hash<KeyType> hf;
-        return hf(x) % array.size();
-    }
-
-    size_t myhashVal(const ValType& y) const
-    {
-        static hash<ValType> hf;
-        return hf(y) % array.size();
-    }
-    //best way need to fix
-    template <typename T>
-    size_t myhash(const T& y) const
-    {
-        static hash<T> hf;
-        return hf(y) % array.size();
-    }
-
 };
-
-#endif
--- TestBiMap.cpp
+++ TestBiMap.cpp
@@ -1,48 +1,110 @@
 #include <iostream>
 #include "BiMap.h"
+
+#include <string>
 using namespace std;
+
+void testBiMap()
+{
+    // Integer to Integer BiMap
+    BiMap<int, int> b1;
+    b1.insert(1, 100);
+    b1.insert(2, 200);
+    b1.insert(3, 300);
+    cout << "b1 after insertions:\n";
+    b1.display();
+
+    cout << "Contains key 2? " << b1.containsKey(2) << endl;
+    cout << "Contains value 300? " << b1.containsVal(300) << endl;
+
+    cout << "Value for key 1: " << b1.getVal(1) << endl;
+    cout << "Removing key 2..." << endl;
+    b1.removeKey(2);
+    b1.display();
+
+    // Integer to String BiMap
+    BiMap<int, std::string> b2;
+    b2.insert(1, "Alice");
+    b2.insert(2, "Bob");
+    b2.insert(3, "Charlie");
+
+    cout << "\nb2 after insertions:\n";
+    b2.display();
+
+    cout << "Contains key 3? " << b2.containsKey(3) << endl;
+    cout << "Contains value 'Bob'? " << b2.containsVal("Bob") << endl;
+
+    cout << "Value for key 1: " << b2.getVal(1) << endl;
+    cout << "Removing value 'Charlie'..." << endl;
+    b2.removeVal("Charlie");
+    b2.display();
+
+    // Char to Float BiMap
+    BiMap<char, float> b3;
+    b3.insert('A', 3.14f);
+    b3.insert('B', 2.71f);
+    b3.insert('C', 1.61f);
+
+    cout << "\nb3 after insertions:\n";
+    b3.display();
+
+    cout << "Contains key 'B'? " << b3.containsKey('B') << endl;
+    cout << "Contains value 3.14? " << b3.containsVal(3.14f) << endl;
+
+    cout << "Value for key 'A': " << b3.getVal('A') << endl;
+    cout << "Removing key 'C'..." << endl;
+    b3.removeKey('C');
+    b3.display();
+}
+
 
     // Simple main
 int main( )
 {
-    BiMap<int,std::string> h1;
-    BiMap<int,std::string> h2;
+    
+    testBiMap();
+    // // HMap<int,std::string> h1;
 
-    h1.ddisplay();
+    // // h1.insert(5,"Five");
+    // // h1.insert(23,"Twenty-Three");
 
-    h1.insert(5,"Five");
-    h1.ddisplay();
+    // // h1.display();
 
-    cout << h1.containsKey(5) << endl;
-    cout << h1.getSize() << endl;
-    cout << "Val corresponding to key 5: " << h1.getVal(5) << endl;
+    // // h1.ddisplay();
 
-    cout << "Key corresponding to Five: " << h1.getKey("Five") << endl;
+    // // cout << h1.getSize() << endl;
 
-    cout << "TEST";
+    // BiMap<int,std::string> b1;
 
-    BiMap<int,int> p;
+    // b1.insert(5,"Five");
 
-    p.insert(5,5);
-    p.ddisplay();
-    cout << "Val corresponding to key 5: " << p.getVal(5) << endl;
-    cout << "Key corresponding to val 5: " << p.getKey(5) << endl;
+    // cout << b1.getSize() << endl;
 
-    p.insert(3,3);
-    p.insert(23,23);
-    cout << p.getSize() << endl;
+    // b1.display();
 
-    p.ddisplay();
-    cout <<"Removing 23\n";
-    p.removeKey(23);
-    //can still get val after removing key 
-    cout << p.getVal(23) << endl;
+    // b1.ddisplay();
 
-    p.ddisplay();
+    // cout << b1.containsKey(5) << endl;
+    // cout << b1.containsVal("Five") << endl;
 
-    cout << "removing value 3\n";
-    p.removeVal(3);
-    p.ddisplay();
-    cout << p.getSize() << endl;
+    // cout << b1.getKey("Five") << endl;
+    // cout << b1.getVal(5) << endl;
+
+    // b1.insert(25,"Twenty-Five");
+
+
+
+    // b1.ddisplay();
+
+    // b1.removeVal("Twenty-Five");
+    // b1.removeKey(5);
+    // b1.ddisplay();
+
+    // for (int i = 0; i < 22; i++)
+    // {
+    //     b1.insert(i,std::to_string(i));
+    // }
+    // b1.ddisplay();
+
     return 0;
 }
